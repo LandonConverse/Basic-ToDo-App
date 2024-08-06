fe081be517740a467db0af060840cd5e076eb4fd
Landon Converse <landonconverse@gmail.com>
2024-08-06 16:15:57 -0500

    Tested and finished addTodo on backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,11 @@
 import express, { Request, Response } from "express";
 import { Pool } from "pg";
+import dotenv from 'dotenv';
 
 const app = express();
+app.use(express.json());
 const PORT = process.env.PORT || 3000;
+
 
 console.log(process.env.DATABASE_URL);
 const pool = new Pool({
@@ -12,10 +15,10 @@ const pool = new Pool({
 type TodoItem = {
   title: string;
   description: string;
-  id: number;
+  id?: number;
 };
 
-const getTodos = async (req: Request, res: Response): Promise<void> => {
+const getTodo = async (req: Request, res: Response): Promise<void> => {
   try {
     const queryResult = await pool.query(
       "SELECT * FROM todo_item ORDER BY id ASC"
@@ -28,8 +31,30 @@ const getTodos = async (req: Request, res: Response): Promise<void> => {
   }
 };
 
-app.get("/todos", getTodos);
-//app.post('/todos', addTodos);
+const createTodo = async (req: Request, res: Response): Promise<void> => {
+    const { title, description }: TodoItem = req.body;
+  
+    if (!title) {
+      res.status(400).json({ error: "Title is required" });
+      return;
+    }
+  
+    try {
+      const result = await pool.query(
+        "INSERT INTO todo_item (title, description) VALUES ($1, $2) RETURNING *",
+        [title, description]
+      );
+      const newTodo: TodoItem = result.rows[0];
+      res.status(201).json({ todo: newTodo });
+    } catch (error) {
+      console.log("Error!", error);
+      res.status(500).json({ error: "Failed to create todo" });
+    }
+  };
+
+
+app.get("/todos", getTodo);
+app.post("/todos", createTodo)
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
